43e5587fc83a5b78e85e2ddbf5183ba9f4fd3058
dea <dea@newrow.com>
2020-12-08 22:01:46 +0400

    remove extra logs

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -12,7 +12,6 @@ const weather = (coordinates, callback) => {
         } else if (response.body.cod && JSON.parse(response.body.cod) === 400) {
             callback(response.body.message);
         } else {
-            console.log('response.body.current.weather[0]', response.body.current.weather[0]);
             const { temp, weather: [{ main, description }] } = response.body.current;
             callback(undefined, `Temperature: ${temp}; Mainly: ${main}, ${description}`);
         }
